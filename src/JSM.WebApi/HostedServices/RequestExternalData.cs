5b27d02754941c35b226b8709658bcd6354e2753
Guilherme Barufi <gm.barufi@gmail.com>
2024-12-26 18:58:34 -0300

    Moving startup request url to appSettings file

--- src/JSM.WebApi/HostedServices/RequestExternalData.cs
+++ src/JSM.WebApi/HostedServices/RequestExternalData.cs
@@ -6,18 +6,23 @@ namespace JSM.WebApi.HostedServices
     internal class RequestExternalData : IHostedService
     {
         private readonly IMediator _mediator;
+        private readonly IConfiguration _configuration;
 
-        public RequestExternalData(IMediator mediator)
+        public RequestExternalData(IMediator mediator, IConfiguration configuration)
         {
             _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
         }
 
         public async Task StartAsync(CancellationToken cancellationToken)
         {
-            var requestUrl = "https://storage.googleapis.com/juntossomosmais-code-challenge/input-backend.csv";
+            var requestUrl = _configuration.GetValue("StartupRequestUrl", string.Empty);
+
+            if (string.IsNullOrEmpty(requestUrl))
+                throw new ArgumentException(nameof(requestUrl));
 
             var httpClient = new HttpClient();
-            var httpResponse = await httpClient.GetAsync(requestUrl, cancellationToken);
+            var httpResponse = await httpClient.GetAsync(requestUrl!, cancellationToken!);
             var command = new CreateCustomerFromCsvCommand { Content = await httpResponse.Content.ReadAsStringAsync(cancellationToken) };
 
             await _mediator.Send(command, cancellationToken);
