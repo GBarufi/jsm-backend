a3cd464190c13047fabe81907f0304c5d741eb38
Guilherme Barufi <gm.barufi@gmail.com>
2024-12-27 14:59:30 -0300

    Creating extension for enums

---
+++ src/JSM.Domain/Extensions/EnumExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace JSM.Domain.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static T? GetEnumValueFromDisplayName<T>(this string displayName) where T : Enum
+        {
+            return Enum.GetValues(typeof(T)).Cast<T>().FirstOrDefault(x => x.GetDisplayName().ToLower() == displayName.ToLower());
+        }
+
+        public static string GetDisplayName(this Enum enumValue)
+        {
+            return enumValue
+                .GetType()
+                .GetMember(enumValue.ToString())[0]?
+                .GetCustomAttribute<DisplayAttribute>()?
+                .Name ?? string.Empty;
+        }
+    }
+}
