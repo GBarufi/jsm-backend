8c6c3add12ffb9fc8e3dc6b7fb346651db57b4ad
Guilherme Barufi <gm.barufi@gmail.com>
2024-12-27 08:59:39 -0300

    Creating the customer mapper and dto equivalent to CSV

---
+++ src/JSM.Application/Dtos/Customers/CustomerCsvDto.cs
@@ -0,0 +1,57 @@
+﻿namespace JSM.Application.Dtos
+{
+    public record CustomerCsvDto
+    {
+        public string? Gender { get; init; }
+        public CustomerName Name { get; init; } = new();
+        public CustomerLocation Location { get; init; } = new();
+        public string? Email { get; init; }
+        public CustomerDate Dob { get; init; } = new();
+        public CustomerDate Registered { get; init; } = new();
+        public string? Phone { get; init; }
+        public string? Cell { get; init; }
+        public CustomerPicture Picture { get; init; } = new();
+
+        public record CustomerName
+        {
+            public string? Title { get; init; }
+            public string? First { get; init; }
+            public string? Last { get; init; }
+        }
+
+        public record CustomerLocation
+        {
+            public string? Street { get; init; }
+            public string? City { get; init; }
+            public string? State { get; init; }
+            public string? PostCode { get; init; }
+            public CustomerLocationCoordinates Coordinates { get; init; } = new();
+            public CustomerLocationTimezone Timezone { get; init; } = new();
+        }
+
+        public record CustomerLocationCoordinates
+        {
+            public string? Latitude { get; init; }
+            public string? Longitude { get; init; }
+        }
+
+        public record CustomerLocationTimezone
+        {
+            public string? Offset { get; init; }
+            public string? Description { get; init; }
+        }
+
+        public record CustomerDate
+        {
+            public DateTime? Date { get; init; }
+            public int? Age { get; init; }
+        }
+
+        public record CustomerPicture
+        {
+            public string? Large { get; init; }
+            public string? Medium { get; init; }
+            public string? Thumbnail { get; init; }
+        }
+    }
+}
---
+++ src/JSM.Application/Mappers/Customers/CustomerCsvMapper.cs
@@ -0,0 +1,34 @@
+﻿using CsvHelper.Configuration;
+using JSM.Application.Dtos;
+
+namespace JSM.Application.Mappers.Customers
+{
+    public class CustomerCsvMapper : ClassMap<CustomerCsvDto>
+    {
+        public CustomerCsvMapper()
+        {
+            Map(x => x.Gender).Index(0);
+            Map(x => x.Name!.Title).Index(1);
+            Map(x => x.Name!.First).Index(2);
+            Map(x => x.Name!.Last).Index(3);
+            Map(x => x.Location!.Street).Index(4);
+            Map(x => x.Location!.City).Index(5);
+            Map(x => x.Location!.State).Index(6);
+            Map(x => x.Location!.PostCode).Index(7);
+            Map(x => x.Location!.Coordinates!.Latitude).Index(8);
+            Map(x => x.Location!.Coordinates!.Latitude).Index(9);
+            Map(x => x.Location!.Timezone!.Offset).Index(10);
+            Map(x => x.Location!.Timezone!.Description).Index(11);
+            Map(x => x.Email).Index(12);
+            Map(x => x.Dob!.Date).Index(13);
+            Map(x => x.Dob!.Age).Index(14);
+            Map(x => x.Registered!.Date).Index(15);
+            Map(x => x.Registered!.Age).Index(16);
+            Map(x => x.Phone).Index(17);
+            Map(x => x.Cell).Index(18);
+            Map(x => x.Picture!.Large).Index(19);
+            Map(x => x.Picture!.Medium).Index(20);
+            Map(x => x.Picture!.Thumbnail).Index(21);
+        }
+    }
+}
