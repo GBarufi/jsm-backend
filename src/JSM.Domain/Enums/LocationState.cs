66868f2d7b02dad5ae11f88bfd1d4dcd4fb7d3df
Guilherme Barufi <gm.barufi@gmail.com>
2024-12-27 15:47:37 -0300

    New enum for states

---
+++ src/JSM.Domain/Enums/LocationState.cs
@@ -0,0 +1,88 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JSM.Domain.Enums
+{
+    public enum LocationState
+    {
+        [Display(Name = "Rio Grande do Sul")]
+        RS = 1,
+
+        [Display(Name = "Santa Catarina")]
+        SC = 2,
+
+        [Display(Name = "Paraná")]
+        PR = 3,
+
+        [Display(Name = "São Paulo")]
+        SP = 4,
+
+        [Display(Name = "Rio de Janeiro")]
+        RJ = 5,
+
+        [Display(Name = "Minas Gerais")]
+        MG = 6,
+
+        [Display(Name = "Espírito Santo")]
+        ES = 7,
+
+        [Display(Name = "Bahia")]
+        BA = 8,
+
+        [Display(Name = "Pernambuco")]
+        PE = 9,
+
+        [Display(Name = "Ceará")]
+        CE = 10,
+
+        [Display(Name = "Rio Grande do Norte")]
+        RN = 11,
+
+        [Display(Name = "Paraíba")]
+        PB = 12,
+
+        [Display(Name = "Piauí")]
+        PI = 13,
+
+        [Display(Name = "Alagoas")]
+        AL = 14,
+
+        [Display(Name = "Sergipe")]
+        SE = 15,
+
+        [Display(Name = "Maranhão")]
+        MA = 16,
+
+        [Display(Name = "Mato Grosso")]
+        MT = 17,
+
+        [Display(Name = "Mato Grosso do Sul")]
+        MS = 18,
+
+        [Display(Name = "Goiás")]
+        GO = 19,
+
+        [Display(Name = "Distrito Federal")]
+        DF = 20,
+
+        [Display(Name = "Amazonas")]
+        AM = 21,
+
+        [Display(Name = "Pará")]
+        PA = 22,
+
+        [Display(Name = "Rondônia")]
+        RO = 23,
+
+        [Display(Name = "Acre")]
+        AC = 24,
+
+        [Display(Name = "Roraima")]
+        RR = 25,
+
+        [Display(Name = "Amapá")]
+        AP = 26,
+
+        [Display(Name = "Tocantins")]
+        TO = 27
+    }
+}
