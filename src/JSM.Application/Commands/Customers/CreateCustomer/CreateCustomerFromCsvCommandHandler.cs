de292ea6633480a97323018595455874d7aef14d
Guilherme Barufi <gm.barufi@gmail.com>
2024-12-27 09:00:40 -0300

    Calling CSV read in handler

--- src/JSM.Application/Commands/Customers/CreateCustomer/CreateCustomerFromCsvCommandHandler.cs
+++ src/JSM.Application/Commands/Customers/CreateCustomer/CreateCustomerFromCsvCommandHandler.cs
@@ -1,11 +1,23 @@
 ﻿using MediatR;
+using JSM.Application.Core.Interfaces;
+using JSM.Application.Mappers.Customers;
+using JSM.Application.Dtos;
 
 namespace JSM.Application.Commands.Customers.CreateCustomer
 {
     public class CreateCustomerFromCsvCommandHandler : IRequestHandler<CreateCustomerFromCsvCommand, int>
     {
+        private readonly ICsvHelper _csvHelper;
+
+        public CreateCustomerFromCsvCommandHandler(ICsvHelper csvHelper)
+        {
+            _csvHelper = csvHelper;
+        }
+
         public async Task<int> Handle(CreateCustomerFromCsvCommand request, CancellationToken cancellationToken)
         {
+            var result = _csvHelper.ImportCsv<CustomerCsvDto, CustomerCsvMapper>(request.Content!).ToList();
+
             await Task.CompletedTask;
 
             return 1;
