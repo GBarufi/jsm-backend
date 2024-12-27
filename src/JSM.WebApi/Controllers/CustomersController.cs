50400b8bcb3024cca49ea3a829304cc7d481c363
Guilherme Barufi <gm.barufi@gmail.com>
2024-12-27 18:50:13 -0300

    Creating customers controller

---
+++ src/JSM.WebApi/Controllers/CustomersController.cs
@@ -0,0 +1,26 @@
+﻿using JSM.Application.Queries.Customers;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JSM.WebApi.Controllers
+{
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private const string _routeBase = "v1/customers";
+        private readonly IMediator _mediator;
+
+        public CustomersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [Route($"{_routeBase}")]
+        public async Task<ActionResult> GetCustomersList([FromQuery] GetCustomersQuery getCustomersQuery)
+        {
+            var result = await _mediator.Send(getCustomersQuery);
+            return Ok(result);
+        }
+    }
+}
