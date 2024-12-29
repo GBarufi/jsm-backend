274684328ee7ce2c8ab9190e0a32ac1e3711f5ec
Guilherme Barufi <gm.barufi@gmail.com>
2024-12-29 15:04:48 -0300

    Binding pagination properties names

--- src/JSM.Application/Core/PaginatedRequest.cs
+++ src/JSM.Application/Core/PaginatedRequest.cs
@@ -1,9 +1,13 @@
-﻿namespace JSM.Application.Core
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JSM.Application.Core
 {
     public record PaginatedRequest<T> : RequestBase<PaginatedResponse<T>>
     {
+        [BindProperty(Name = "page")]
         public int? Page { get; init; } = 0;
 
+        [BindProperty(Name = "size")]
         public int? Size { get; init; } = 10;
 
         public override bool IsValid() => true;
