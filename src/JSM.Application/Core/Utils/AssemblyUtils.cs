b9f0ab0e353abab885d261f3cfbbc2496176fb72
Guilherme Barufi <gm.barufi@gmail.com>
2024-12-29 20:01:50 -0300

    Enabling swagger xml comments

---
+++ src/JSM.Application/Core/Utils/AssemblyUtils.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+namespace JSM.Application.Core.Utils
+{
+    public static class AssemblyUtils
+    {
+        public static Assembly GetAssembly()
+        {
+            return Assembly.GetExecutingAssembly();
+        }
+    }
+}
--- src/JSM.WebApi/Program.cs
+++ src/JSM.WebApi/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.ConfigureSwagger();
 builder.Services.ConfigureDependencyInjection();
 builder.Services.ConfigureMediator();
 builder.Services.AddRouting(options => options.LowercaseUrls = true);
